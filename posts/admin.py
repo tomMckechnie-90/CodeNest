e3952f6829bad13d5e6f5db6179044de13f92c6c
Tom McKechnie <tommckechnie8@gmail.com>
2025-01-10 14:57:44 +0000

    Updated the posts model

--- posts/admin.py
+++ posts/admin.py
@@ -4,8 +4,9 @@ from posts.models import Post
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')
-    search_fields = ('title',)
-    list_filter = ('created_at',)
+    list_display = ('title', 'slug', 'status', 'created_at')
+    search_fields = ['title', 'content']
+    list_filter = ('status','created_at',)
+    prepopulated_fields = {'slug': ('title',)}
 
     
--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200)
+    author = models.ForeignKey
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.title} | written by {self.author}"
