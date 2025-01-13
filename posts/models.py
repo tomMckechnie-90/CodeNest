13ba7c6b01996d0f8150ba5a96e3498e3698891c
Tom McKechnie <tommckechnie8@gmail.com>
2025-01-13 22:25:39 +0000

    Enable DEBUG mode for development, update Post model to auto-generate slug, and add error handling for missing slugs in templates.

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -9,12 +10,17 @@ STATUS = ((0, "Draft"), (1, "Published"))
 # The post model
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', default=1)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:  # Generate slug only if it doesn't exist
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
