39b4f32771fa22143cdc93f386bb6cd427ec2361
Tom McKechnie <tommckechnie8@gmail.com>
2025-01-23 13:49:21 +0000

    Style edit post form and other html templates for improved layout

--- posts/forms.py
+++ posts/forms.py
@@ -13,9 +13,9 @@ class CommentForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'image',]
+        fields = ['title', 'image', 'content',]
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter post title'}),
-            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Write your post here...'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write your post here...'}),
             'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
