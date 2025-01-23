1bddec41beac972a248745b792b99cf531a1ea20
Tom McKechnie <tommckechnie8@gmail.com>
2025-01-23 15:57:54 +0000

    Update user profile and post creation templates for improved layout

--- user_profiles/forms.py
+++ user_profiles/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import UserProfile
 
+from django import forms
+from .models import UserProfile
+
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['bio', 'profile_picture']
+        fields = ['first_name', 'last_name', 'date_of_birth', 'gender', 'bio', 'profile_picture']
+        widgets = {
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
+        }
--- user_profiles/models.py
+++ user_profiles/models.py
@@ -5,7 +5,18 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class UserProfile(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    first_name = models.CharField(max_length=50, default="John")
+    last_name = models.CharField(max_length=50, default="Doe")
+    date_of_birth = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
     profile_picture = CloudinaryField('image', default='default-profile_mzguyn')
     created_at = models.DateTimeField(auto_now_add=True)
